332db71c0f1387b3e145df59235cf456a8ef5f1f
Tyty-land <ss-200909@mail.ru>
2026-05-22 02:15:01 +0400

    run.sh exec

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd /home/ubu/app-code
-# Вытаскиваем хэш
 export DEPLOY_REF=$(git rev-parse HEAD)
-# Запускаем сайт
-/home/ubu/app-venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8181
+exec /home/ubu/app-venv/bin/uvicorn app.main:app --host 0.0.0.0 --port 8181
